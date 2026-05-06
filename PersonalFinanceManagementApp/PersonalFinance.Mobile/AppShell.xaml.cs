7bd53731376c52de632772a412b37a29ec9569d4
Kindy10 <bahmamadoukindy349@gmail.com>
2026-05-06 17:21:35 +0300

    LoginPage and DashboardPage added, see #3

--- PersonalFinanceManagementApp/PersonalFinance.Mobile/AppShell.xaml.cs
+++ PersonalFinanceManagementApp/PersonalFinance.Mobile/AppShell.xaml.cs
@@ -5,6 +5,7 @@
         public AppShell()
         {
             InitializeComponent();
+            //await Shell.Current.GoToAsync("//login");
         }
     }
 }
---
+++ PersonalFinanceManagementApp/PersonalFinance.Mobile/ViewModels/LoginViewModel.cs
@@ -0,0 +1,46 @@
+﻿using PersonalFinance.Mobile.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PersonalFinance.Mobile.ViewModels
+{
+    public  class LoginViewModel
+    {
+        private readonly AuthService _authService;
+
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        public ICommand LoginCommand { get; }
+        
+        public LoginViewModel()
+        {
+            _authService = new AuthService();
+            LoginCommand = new Command(async () => await Login());
+        }
+
+        private async Task Login()
+        {
+            try
+            {
+                 var result = await _authService.LoginAsync(Email, Password);
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Success",
+                    $"Welcome {result.Name}",
+                    "OK");
+
+                //Navigate after login
+                await Shell.Current.GoToAsync("//dashboard");
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+    }
+}
