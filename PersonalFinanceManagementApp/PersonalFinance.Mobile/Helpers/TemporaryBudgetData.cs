54076ca36f96ceeb5410d83a4ab0f1c11bdf8690
Kindy10 <bahmamadoukindy349@gmail.com>
2026-05-17 01:46:31 +0300

    Work in progress before rebase

---
+++ PersonalFinanceManagementApp/PersonalFinance.Mobile/Helpers/TemporaryBudgetData.cs
@@ -0,0 +1,14 @@
+﻿using PersonalFinance.Shared.DTOs.Budgets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinance.Mobile.Helpers
+{
+    public  class TemporaryBudgetData
+    {
+        public static BudgetDto SelectedBudget { get; set; }
+    }
+}
